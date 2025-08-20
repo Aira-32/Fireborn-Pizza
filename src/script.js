1951006664ffe7ecf345e10e4c08cfa891351aa9
Aira-32 <aurelia.tusa09@gmail.com>
2025-08-20 21:10:15 +0200

    Added the lightbox logic for the gallery

--- src/script.js
+++ src/script.js
@@ -66,3 +66,64 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+
+const lightboxGallery = document.getElementById('lightbox');
+
+const cardImgsGallery = document.querySelectorAll('.card-img-gallery')
+
+const images = [
+
+];
+
+cardImgsGallery.forEach(card => {
+    images.push(card.querySelector('img').src)
+});
+
+
+cardImgsGallery.forEach(card => {
+    card.addEventListener('click', (e) => {
+        lightboxGallery.classList.remove('hidden');
+        let imgSrc = e.target.closest('img').src;
+        lightboxGallery.querySelector('img').src = imgSrc;
+
+        lightboxGallery.querySelector('.fa-x').addEventListener('click', () => lightboxGallery.classList.add('hidden'));
+
+        lightboxGallery.querySelectorAll('.arrow').forEach(arrow => {
+            arrow.addEventListener('click', (e) => {
+                let indexOfCurrentImg = images.findIndex(src => src === imgSrc);
+
+                // because the findIndex method returns -1 when he doesn't find anything
+                if ( indexOfCurrentImg === -1 ) {
+                    indexOfCurrentImg = 0;
+                };
+
+                // check if it's the right or left arrow
+                if( arrow === lightboxGallery.querySelector('.arrow') ) { 
+
+                    // if it's the first img and we want to go back
+                    if (indexOfCurrentImg === 0) {
+                        lightboxGallery.querySelector('img').src = images[images.length - 1];
+
+                    } else {
+                        lightboxGallery.querySelector('img').src = images[indexOfCurrentImg - 1] 
+                    }
+
+                }
+                else {
+
+                    // if it's the last img and we want to go forward
+                    if (indexOfCurrentImg === images.length - 1) {
+                        lightboxGallery.querySelector('img').src = images[0];
+
+                    } else {
+                        lightboxGallery.querySelector('img').src = images[indexOfCurrentImg + 1] 
+                    }                }
+
+                // update the src to the new img
+                imgSrc = lightboxGallery.querySelector('img').src
+            })
+        });
+
+    });
+});
